2c62fab68bd6a70cdc192d275ed98878361676d7
Oliver Brown <oliver.brown@chelsea-apps.com>
2015-02-19 14:04:30 +0000

    Moved counter into it's own project, now I know what it does.

--- Gaillard.SharpCover.Counter/Counter.cs
+++ Gaillard.SharpCover.Counter/Counter.cs
@@ -24,7 +24,7 @@ namespace Gaillard.SharpCover
         public static void Count(string pathPrefix, int index)
         {
             if (path == null) {
-                path = pathPrefix + "|" + Process.GetCurrentProcess().Id + "|" + Thread.CurrentThread.ManagedThreadId;
+				path = pathPrefix + "|" + Process.GetCurrentProcess().Id + "|" + Thread.CurrentThread.ManagedThreadId;
                 indexes = new HashSet<int>();
                 writer = new BinaryWriter(File.Open(path, FileMode.CreateNew));
             }
