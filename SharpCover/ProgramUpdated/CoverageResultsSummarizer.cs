3d784c9e150e7f99e87a8ff3d9625ad31efa9c83
aallfredo <paintba11>
2015-12-10 14:56:30 -0800

    CoverageResultsSummarizerInProgress

--- SharpCover/ProgramUpdated/CoverageResultsSummarizer.cs
+++ SharpCover/ProgramUpdated/CoverageResultsSummarizer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,33 @@ namespace ProgramUpdated
 {
     public class CoverageResultsSummarizer
     {
+
+
+
+
+        public void Fill(string path)
+        {
+            var allText = File.ReadAllText(path);
+            CommonFill(allText);
+        }
+
+        private void CommonFill(string content)
+        {
+            var breakInLine = content.Split('\n');
+            foreach (var oneLine in breakInLine.Where(s=>!String.IsNullOrWhiteSpace(s)))
+            {
+                if (oneLine.Contains("MISS !"))
+                {
+                    //We need To Process the miss.
+                }
+            }
+        }
+
+        public void Parse(string content)
+        {
+            CommonFill(content);
+        }
+
+
     }
 }
